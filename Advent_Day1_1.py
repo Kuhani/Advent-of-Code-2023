648b814bfc0c2b7c11e490d7dfd2887157d8d2d7
Kuhani <67787617+Kuhani@users.noreply.github.com>
2023-12-05 17:12:32 +0200

    Update Advent_Day1_1.py

--- Advent_Day1_1.py
+++ Advent_Day1_1.py
@@ -1,14 +1,13 @@
 result = 0
 calibration_value = []
-values = ["0","1","2","3","4","5","6","7","8","9"]
 file = open("day1_input.txt", "r")
 input = file.read()
 input_list = input.split('\n')
 for line in input_list:
     for value in line:
-        if value in values:
+        if value.isnumeric() == True:
             calibration_value.append(value)
             result_line = int(calibration_value[0])*10 + int(calibration_value[-1])
     result += result_line
-    calibration_value=[]
+    calibration_value = []
 print(result)
